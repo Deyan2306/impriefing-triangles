c19eb7162570b92fe0cdbfc5c71450311df3968d
D3Y4N <103333790+Deyan2306@users.noreply.github.com>
2023-11-20 22:32:10 +0200

    i utre 6

---
+++ marti-dm/marti-dm/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace marti_dm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String, Rectangle> rectanglesHolder = new Dictionary<string, Rectangle>();
+
+            int[] tokens = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            
+            int numberOfRectangles = tokens[0];
+            int numberOfChecks = tokens[1];
+
+            for (int iterator = 0; iterator < numberOfRectangles; iterator++)
+            {
+                string[] rectangleTokens = Console.ReadLine().Split(' ').ToArray();
+                string id = rectangleTokens[0];
+                rectanglesHolder[id] = new Rectangle(
+                    id, 
+                    int.Parse(rectangleTokens[1]), 
+                    int.Parse(rectangleTokens[2]), 
+                    int.Parse(rectangleTokens[3]), 
+                    int.Parse(rectangleTokens[4]));
+            }
+
+            for (int iterator = 0; iterator < numberOfChecks; iterator++)
+            {
+                string[] rectanglesToCheck = Console.ReadLine().Split(' ');
+                Rectangle firstRectangle = rectanglesHolder[rectanglesToCheck[0]];
+                Rectangle secondRectangle = rectanglesHolder[rectanglesToCheck[1]];
+
+                Console.WriteLine(firstRectangle.impriefs(secondRectangle) || secondRectangle.impriefs(firstRectangle));
+            }
+        }
+    }
+}
---
+++ marti-dm/marti-dm/Rectangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace marti_dm
+{
+    internal class Rectangle
+    {
+        private string id;
+        public string Id { get; set; }
+
+        private int width;
+        public int Width { get; set; }
+
+        private int height;
+        public int Height { get; set; }
+
+        private int leftCornerX;
+        private int leftCornerY;
+        public int LeftCornerX { get; set; }
+        public int LeftCornerY { get; set; }
+
+        public Rectangle(string id, int width, int height, int leftCornerX, int leftCornerY)
+        {
+            this.Id = id;
+            this.Width = width;
+            this.Height = height;
+            this.LeftCornerX = leftCornerX;
+            this.LeftCornerY = leftCornerY;
+        }
+
+        public bool impriefs(Rectangle rectangle) 
+        {
+            // all corner coordinates of this object
+            int thisLeftX = this.LeftCornerX;
+            int thisTopY = this.LeftCornerY;
+            int thisRightX = this.LeftCornerX + this.Width;
+            int thisBottomY = this.LeftCornerY + this.Height;
+            
+            // all corner coordinates of the other object
+            int otherLeftX = rectangle.LeftCornerX;
+            int otherTopY = rectangle.LeftCornerY;
+            int otherRightX = rectangle.LeftCornerX + rectangle.Width;
+            int otherBottomY = rectangle.LeftCornerY + rectangle.Height;
+
+            // check if the two rectangles are on top of each other based on the top left coordeinate
+            if ((thisLeftX <= otherLeftX) && (otherLeftX < thisRightX))     { return true; }
+            else if ((thisTopY <= otherTopY) && (otherTopY < thisBottomY))  { return true; }
+
+            return false;
+        }
+    }
+}
